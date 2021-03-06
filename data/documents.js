6bdc94b6c8ea81296b38576353c6d53fe26692a5
Devon Wolfkiel <devonwolf@tutanota.com>
2021-03-06 12:24:52 -0800

    Initial database setup

---
+++ data/documents.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    title: 'The First Document',
+    body_text: 'This is a document with some text in the body.',
+  },
+  {
+    title: 'Untitled',
+    body_text: 'There is no content here.',
+  },
+  {
+    title: 'See Spot Type',
+    body_text: 'oiejfoskdfdfOSDO SODOSDNRF9843OUEWJ - Spot is a dog and is bad at typing.',
+  }
+];
